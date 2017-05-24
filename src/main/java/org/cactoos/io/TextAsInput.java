4714000a8c6de5d7e5ab77e6c2d460c1110d28d9
g4s8 <g4s8.public@gmail.com>
2017-05-25 00:47:26 +0400

    Use charset encoding for text & input

--- src/main/java/org/cactoos/io/TextAsInput.java
+++ src/main/java/org/cactoos/io/TextAsInput.java
@@ -26,6 +26,7 @@ package org.cactoos.io;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import org.cactoos.Input;
 import org.cactoos.Text;
 
@@ -46,16 +47,35 @@ public final class TextAsInput implements Input {
     private final Text source;
 
     /**
-     * Ctor.
+     * Text charset.
+     */
+    private final Charset charset;
+
+    /**
+     * New {@link TextAsInput} with default charset.
+     *
      * @param text The text
      */
     public TextAsInput(final Text text) {
+        this(text, Charset.defaultCharset());
+    }
+
+    /**
+     * New {@link TextAsInput} with specified charset.
+     *
+     * @param text The text
+     * @param charset Text charset
+     */
+    public TextAsInput(final Text text, final Charset charset) {
         this.source = text;
+        this.charset = charset;
     }
 
     @Override
     public InputStream open() throws IOException {
-        return new ByteArrayInputStream(this.source.asString().getBytes());
+        return new ByteArrayInputStream(
+            this.source.asString().getBytes(this.charset)
+        );
     }
 
 }
