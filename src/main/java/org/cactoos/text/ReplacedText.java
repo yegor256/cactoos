118b4f9eb00457e7a20022e350390bb46a8223c6
mehyil <mehyil@netas.com.tr>
2017-06-02 14:42:15 +0300

    Checkstyle fix

--- src/main/java/org/cactoos/text/ReplacedText.java
+++ src/main/java/org/cactoos/text/ReplacedText.java
@@ -1,8 +1,30 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.cactoos.text;
 
-import org.cactoos.Text;
-
 import java.io.IOException;
+import org.cactoos.Text;
 
 /**
  * Replace the Text.
@@ -11,7 +33,7 @@ import java.io.IOException;
  * @version $Id$
  * @since 0.1
  */
-public class ReplacedText implements Text {
+public final class ReplacedText implements Text {
 
     /**
      * The text.
@@ -21,28 +43,29 @@ public class ReplacedText implements Text {
     /**
      * The old char.
      */
-    private String oldChar;
+    private final String oldchar;
 
     /**
      * The new char.
      */
-    private String newChar;
+    private final String newchar;
 
     /**
      * Ctor.
-     *
-     * @param text    The text
-     * @param oldChar The old char
-     * @param newChar The new char
+     * @param text The text
+     * @param oldchar The old char
+     * @param newchar The new char
      */
-    public ReplacedText(final Text text, String oldChar, String newChar) {
+    public ReplacedText(final Text text, final String oldchar, final String
+        newchar) {
         this.origin = text;
-        this.oldChar = oldChar;
-        this.newChar = newChar;
+        this.oldchar = oldchar;
+        this.newchar = newchar;
     }
 
     @Override
     public String asString() throws IOException {
-        return origin.asString().replace(oldChar, newChar);
+        return this.origin.asString().replace(this.oldchar, this.newchar);
     }
 }
+
--- src/test/java/org/cactoos/text/ReplacedTextTest.java
+++ src/test/java/org/cactoos/text/ReplacedTextTest.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.cactoos.text;
 
 import org.cactoos.TextHasString;
@@ -6,6 +29,7 @@ import org.junit.Test;
 
 /**
  * Test case for {@link ReplacedText}.
+ *
  * @author Mehmet Yildirim (memoyil@gmail.com)
  * @version $Id$
  * @since 0.1
@@ -16,9 +40,12 @@ public final class ReplacedTextTest {
     @Test
     public void replaceText() {
         MatcherAssert.assertThat(
-                "Can't replace a text",
-                new ReplacedText(new StringAsText("Hello!"), "ello", "i"),
-                new TextHasString("Hi!")
+            "Can't replace a text",
+            new ReplacedText(
+                new StringAsText("Hello!"),
+                "ello", "i"
+            ),
+            new TextHasString("Hi!")
         );
     }
 }
