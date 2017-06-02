2cff1aa8e9a5054c2bef1cd873f2903ba866f140
Fabricio Cabral <fabriciofx@gmail.com>
2017-06-02 18:59:28 -0300

    #49: Abbreviated a text

---
+++ src/main/java/org/cactoos/text/AbbreviatedText.java
@@ -0,0 +1,93 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cactoos.text;
+
+import java.io.IOException;
+import org.cactoos.Text;
+
+/**
+ * Abbreviates a Text using ellipses.
+ *
+ * <p>There is no thread-safety guarantee.
+ *
+ * @author Fabricio Cabral (fabriciofx@gmail.com)
+ * @version $Id$
+ * @since 0.2
+ */
+public final class AbbreviatedText implements Text {
+
+    /**
+     * The text.
+     */
+    private final Text origin;
+
+    /**
+     * The initial position in the text.
+     */
+    private final int offset;
+
+    /**
+     * The max width of the abbreviated text.
+     */
+    private final int width;
+
+    /**
+     * Ctor.
+     * @param text The Text
+     */
+    public AbbreviatedText(final Text text) {
+        // @checkstyle MagicNumber (1 line)
+        this(text, 80);
+    }
+
+    /**
+     * Ctor.
+     * @param text The Text
+     * @param width Width of the result string
+     */
+    public AbbreviatedText(final Text text, final int width) {
+        // @checkstyle MagicNumber (1 line)
+        this(text, 0, width);
+    }
+
+    /**
+     * Ctor.
+     * @param text The Text
+     * @param offset Text position where to start
+     * @param width Width of the result string
+     */
+    public AbbreviatedText(final Text text, final int offset, final int width) {
+        this.origin = text;
+        this.offset = offset;
+        this.width = width;
+    }
+
+    @Override
+    public String asString() throws IOException {
+        return new FormattedText(
+            "%s...",
+            this.origin.asString().substring(this.offset, this.width)
+        ).asString();
+    }
+}
