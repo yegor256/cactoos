5fcdcb667f546e900d084c4c501c7582c06957bb
yegor256 <yegor256@gmail.com>
2017-06-19 10:43:29 +0200

    #176: MapAsProperties

---
+++ src/test/java/org/cactoos/list/MapAsPropertiesTest.java
@@ -0,0 +1,60 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cactoos.list;
+
+import java.util.AbstractMap;
+import java.util.Properties;
+import org.cactoos.ScalarHasValue;
+import org.cactoos.func.FuncAsMatcher;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+/**
+ * Test case for {@link MapAsProperties}.
+ *
+ * @author Yegor Bugayenko (yegor256@gmail.com)
+ * @version $Id$
+ * @since 0.7
+ * @checkstyle JavadocMethodCheck (500 lines)
+ */
+public final class MapAsPropertiesTest {
+
+    @Test
+    public void convertsMapToProperties() {
+        MatcherAssert.assertThat(
+            "Can't convert map to properties",
+            new MapAsProperties(
+                new IterableAsMap<Integer, String>(
+                    new AbstractMap.SimpleEntry<>(0, "hello, world"),
+                    new AbstractMap.SimpleEntry<>(1, "how are you?")
+                )
+            ),
+            new ScalarHasValue<>(
+                new FuncAsMatcher<Properties>(
+                    props -> props.getProperty("0").endsWith(", world")
+                )
+            )
+        );
+    }
+}
