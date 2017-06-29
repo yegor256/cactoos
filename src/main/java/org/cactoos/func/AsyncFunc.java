2f1fe9408c917c4ef9288de64098a2d79a591d4d
yegor256 <yegor256@gmail.com>
2017-06-29 13:29:54 +0300

    #265: AsyncFunc

---
+++ src/main/java/org/cactoos/func/AsyncFunc.java
@@ -0,0 +1,70 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cactoos.func;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import org.cactoos.Func;
+
+/**
+ * Func that runs in the background.
+ *
+ * <p>If you want your piece of code to be executed in the background,
+ * use {@link AsyncFunc} as following:</p>
+ *
+ * <pre> new AsyncFunc(
+ *   input -&gt; input.length()
+ * ).apply("Hello, world!").get();</pre>
+ *
+ * <p>There is no thread-safety guarantee.
+ *
+ * @author Yegor Bugayenko (yegor256@gmail.com)
+ * @version $Id$
+ * @param <X> Type of input
+ * @param <Y> Type of output
+ * @since 0.10
+ */
+public final class AsyncFunc<X, Y> implements Func<X, Future<Y>> {
+
+    /**
+     * The func.
+     */
+    private final Func<X, Y> func;
+
+    /**
+     * Ctor.
+     * @param fnc The proc
+     */
+    public AsyncFunc(final Func<X, Y> fnc) {
+        this.func = fnc;
+    }
+
+    @Override
+    public Future<Y> apply(final X input) throws Exception {
+        return Executors.newSingleThreadExecutor().submit(
+            () -> this.func.apply(input)
+        );
+    }
+
+}
