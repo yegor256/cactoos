87898f81cc5090a058f578edbb87bead9a6147d0
mehyil <mehyil@netas.com.tr>
2017-07-24 11:40:20 +0300

    Adding test cases for illegal char #225

--- src/test/java/org/cactoos/codec/Base64ToInputTest.java
+++ src/test/java/org/cactoos/codec/Base64ToInputTest.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- *
+ * <p>
  * Copyright (c) 2017 Yegor Bugayenko
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,6 +28,7 @@ import java.io.StringReader;
 import org.cactoos.InputHasContent;
 import org.cactoos.io.ReaderAsInput;
 import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 /**
@@ -47,6 +48,19 @@ public final class Base64ToInputTest {
             new Base64ToInput(
                 new ReaderAsInput(
                     new StringReader("SGVsbG8h")
+                )
+            ),
+            new InputHasContent("Hello!")
+        );
+    }
+
+    @Test(expected = DecodingException.class)
+    public void illegalCharacter() throws IOException {
+        MatcherAssert.assertThat(
+            "Can't decode a text",
+            new Base64ToInput(
+                new ReaderAsInput(
+                    new StringReader("!@#$%^&*()")
                 )
             ),
             new InputHasContent("Hello!")
