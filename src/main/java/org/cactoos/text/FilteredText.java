19d4eb3e1767bec67496732fc6a6a060b1f2da44
Sven Diedrichsen <sdiedrichsen@yahoo.de>
2017-07-24 17:14:19 +0200

    Added FilteredText

---
+++ src/main/java/org/cactoos/text/FilteredText.java
@@ -0,0 +1,76 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cactoos.text;
+
+import java.io.IOException;
+import org.cactoos.Func;
+import org.cactoos.Text;
+import org.cactoos.func.IoCheckedFunc;
+
+/**
+ * Text filtered by a {@link org.cactoos.Func} working as a filter.
+ *
+ * @author Sven Diedrichsen (sven.diedrichsen@gmail.com)
+ * @version $Id$
+ * @since 24.07.17
+ */
+public class FilteredText implements Text {
+
+    /**
+     * Original text to filter.
+     */
+    private final Text origin;
+    /**
+     * The {@link Func} as {@link IoCheckedFunc} to use for filtering.
+     */
+    private final IoCheckedFunc<Character, Boolean> filter;
+
+    /**
+     * Ctor.
+     * @param origin The original text to filter.
+     * @param filter The filter to use.
+     */
+    public FilteredText(final Text origin,
+        final Func<Character, Boolean> filter) {
+        this.origin = origin;
+        this.filter = new IoCheckedFunc<>(filter);
+    }
+
+    @Override
+    public final String asString() throws IOException {
+        final StringBuilder filtered = new StringBuilder();
+        for (final char character : this.origin.asString().toCharArray()) {
+            if (this.filter.apply(character)) {
+                filtered.append(character);
+            }
+        }
+        return filtered.toString();
+    }
+
+    @Override
+    public final int compareTo(final Text text) {
+        return new UncheckedText(this).compareTo(text);
+    }
+
+}
---
+++ src/test/java/org/cactoos/text/FilteredTextTest.java
@@ -0,0 +1,77 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cactoos.text;
+
+import org.cactoos.TextHasString;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+/**
+ * Tests for {@link FilteredText}.
+ * @author Sven Diedrichsen (sven.diedrichsen@gmail.com)
+ * @version $Id$
+ * @since 24.07.17
+ * @checkstyle JavadocMethodCheck (500 lines)
+ */
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+public class FilteredTextTest {
+
+    @Test
+    public final void testFiltering() throws Exception {
+        MatcherAssert.assertThat(
+            "Can't filter a text",
+            new FilteredText(
+                new StringAsText("abc"),
+                input -> !Character.valueOf('c').equals(input)
+            ),
+            new TextHasString("ab")
+        );
+    }
+
+    @Test
+    public final void testFilteringEmptyText() throws Exception {
+        MatcherAssert.assertThat(
+            "Can't filter empty text",
+            new FilteredText(
+                new StringAsText(""),
+                input -> !Character.valueOf('c').equals(input)
+            ),
+            new TextHasString("")
+        );
+    }
+
+    @Test
+    public final void testComparing() throws Exception {
+        MatcherAssert.assertThat(
+            "Can't compare filtered text",
+            new FilteredText(
+                new StringAsText("abc"),
+                input -> !Character.valueOf('c').equals(input)
+            ).compareTo(new StringAsText("ab")),
+            Matchers.is(0)
+        );
+    }
+
+}
