89389a831bb5af27501c907c71ec8db8782044da
Yegor Bugayenko <yegor256@gmail.com>
2017-08-08 17:59:43 +0300

    #394 more ctors

--- src/main/java/org/cactoos/io/TeeInput.java
+++ src/main/java/org/cactoos/io/TeeInput.java
@@ -26,9 +26,14 @@ package org.cactoos.io;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
+import org.cactoos.Bytes;
 import org.cactoos.Input;
 import org.cactoos.Output;
+import org.cactoos.Text;
 
 /**
  * Input to Output copying pipe.
@@ -53,42 +58,122 @@ public final class TeeInput implements Input {
 
     /**
      * Ctor.
-     * @param input The source
-     * @param output The output file
-     * @since 0.5
+     * @param url The source
+     * @param path The output file
+     * @since 0.13.3
      */
-    public TeeInput(final Path input, final Path output) {
-        this(new InputOf(input), new OutputTo(output));
+    public TeeInput(final URL url, final Path path) {
+        this(new InputOf(url), new OutputTo(path));
+    }
+
+    /**
+     * Ctor.
+     * @param url The source
+     * @param file The output file
+     * @since 0.13.3
+     */
+    public TeeInput(final URL url, final File file) {
+        this(new InputOf(url), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param url The source
+     * @param output The output file
+     * @since 0.13.3
+     */
+    public TeeInput(final URL url, final Output output) {
+        this(new InputOf(url), output);
+    }
+
+    /**
+     * Ctor.
+     * @param uri The source
+     * @param path The output file
+     * @since 0.13.3
+     */
+    public TeeInput(final URI uri, final Path path) {
+        this(new InputOf(uri), new OutputTo(path));
+    }
+
+    /**
+     * Ctor.
+     * @param uri The source
+     * @param file The output file
+     * @since 0.13.3
+     */
+    public TeeInput(final URI uri, final File file) {
+        this(new InputOf(uri), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param uri The source
+     * @param output The output file
+     * @since 0.13.3
+     */
+    public TeeInput(final URI uri, final Output output) {
+        this(new InputOf(uri), output);
     }
 
     /**
      * Ctor.
      * @param input The source
-     * @param output The output file
+     * @param path The output file
      * @since 0.5
      */
-    public TeeInput(final Path input, final File output) {
-        this(new InputOf(input), new OutputTo(output));
+    public TeeInput(final Path input, final Path path) {
+        this(new InputOf(input), new OutputTo(path));
     }
 
     /**
      * Ctor.
      * @param input The source
-     * @param output The output file
+     * @param file The output file
      * @since 0.5
      */
-    public TeeInput(final File input, final File output) {
-        this(new InputOf(input), new OutputTo(output));
+    public TeeInput(final Path input, final File file) {
+        this(new InputOf(input), new OutputTo(file));
     }
 
     /**
      * Ctor.
      * @param input The source
-     * @param output The output file
+     * @param output The output
+     * @since 0.13.3
+     */
+    public TeeInput(final Path input, final Output output) {
+        this(new InputOf(input), output);
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param file The output file
      * @since 0.5
      */
-    public TeeInput(final File input, final Path output) {
-        this(new InputOf(input), new OutputTo(output));
+    public TeeInput(final File input, final File file) {
+        this(new InputOf(input), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param path The output file
+     * @since 0.5
+     */
+    public TeeInput(final File input, final Path path) {
+        this(new InputOf(input), new OutputTo(path));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param output The output
+     * @since 0.13.3
+     */
+    public TeeInput(final File input, final Output output) {
+        this(new InputOf(input), output);
     }
 
     /**
@@ -105,6 +190,30 @@ public final class TeeInput implements Input {
      * Ctor.
      * @param input The source
      * @param file The output file
+     * @param charset The charset
+     * @since 0.13.3
+     */
+    public TeeInput(final CharSequence input, final File file,
+        final Charset charset) {
+        this(new InputOf(input, charset), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param file The output file
+     * @param charset The charset
+     * @since 0.13.3
+     */
+    public TeeInput(final CharSequence input, final File file,
+        final CharSequence charset) {
+        this(new InputOf(input, charset), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param file The output file
      * @since 0.5
      */
     public TeeInput(final CharSequence input, final Path file) {
@@ -115,10 +224,24 @@ public final class TeeInput implements Input {
      * Ctor.
      * @param input The source
      * @param file The output file
-     * @since 0.5
+     * @param charset The charset
+     * @since 0.13.3
      */
-    public TeeInput(final byte[] input, final Path file) {
-        this(new InputOf(input), new OutputTo(file));
+    public TeeInput(final CharSequence input, final Path file,
+        final Charset charset) {
+        this(new InputOf(input, charset), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param file The output file
+     * @param charset The charset
+     * @since 0.13.3
+     */
+    public TeeInput(final CharSequence input, final Path file,
+        final CharSequence charset) {
+        this(new InputOf(input, charset), new OutputTo(file));
     }
 
     /**
@@ -129,6 +252,312 @@ public final class TeeInput implements Input {
      */
     public TeeInput(final CharSequence input, final Output output) {
         this(new InputOf(input), output);
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param output The target
+     * @param charset The charset
+     * @since 0.13.3
+     */
+    public TeeInput(final CharSequence input, final Output output,
+        final Charset charset) {
+        this(new InputOf(input, charset), output);
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param output The target
+     * @param charset The charset
+     * @since 0.13.3
+     */
+    public TeeInput(final CharSequence input, final Output output,
+        final CharSequence charset) {
+        this(new InputOf(input, charset), output);
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param path The output path
+     * @since 0.5
+     */
+    public TeeInput(final byte[] input, final Path path) {
+        this(new InputOf(input), new OutputTo(path));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param file The output file
+     * @since 0.5
+     */
+    public TeeInput(final byte[] input, final File file) {
+        this(new InputOf(input), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param output The output file
+     * @since 0.5
+     */
+    public TeeInput(final byte[] input, final Output output) {
+        this(new InputOf(input), output);
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param path The output path
+     * @since 0.5
+     */
+    public TeeInput(final char[] input, final Path path) {
+        this(new InputOf(input), new OutputTo(path));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param path The output path
+     * @param charset Charset
+     * @since 0.13.3
+     */
+    public TeeInput(final char[] input, final Path path,
+        final Charset charset) {
+        this(new InputOf(input, charset), new OutputTo(path));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param path The output path
+     * @param charset Charset
+     * @since 0.13.3
+     */
+    public TeeInput(final char[] input, final Path path,
+        final CharSequence charset) {
+        this(new InputOf(input, charset), new OutputTo(path));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param file The output file
+     * @since 0.5
+     */
+    public TeeInput(final char[] input, final File file) {
+        this(new InputOf(input), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param file The output file
+     * @param charset Charset
+     * @since 0.13.3
+     */
+    public TeeInput(final char[] input, final File file,
+        final Charset charset) {
+        this(new InputOf(input, charset), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param file The output file
+     * @param charset Charset
+     * @since 0.13.3
+     */
+    public TeeInput(final char[] input, final File file,
+        final CharSequence charset) {
+        this(new InputOf(input, charset), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param output The output file
+     * @since 0.5
+     */
+    public TeeInput(final char[] input, final Output output) {
+        this(new InputOf(input), output);
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param output The output file
+     * @param charset Charset
+     * @since 0.13.3
+     */
+    public TeeInput(final char[] input, final Output output,
+        final Charset charset) {
+        this(new InputOf(input, charset), output);
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param output The output file
+     * @param charset Charset
+     * @since 0.13.3
+     */
+    public TeeInput(final char[] input, final Output output,
+        final CharSequence charset) {
+        this(new InputOf(input, charset), output);
+    }
+
+    /**
+     * Ctor.
+     * @param text The source
+     * @param path The output path
+     * @since 0.13.3
+     */
+    public TeeInput(final Text text, final Path path) {
+        this(new InputOf(text), new OutputTo(path));
+    }
+
+    /**
+     * Ctor.
+     * @param text The source
+     * @param path The output path
+     * @param charset Charset
+     * @since 0.13.3
+     */
+    public TeeInput(final Text text, final Path path, final Charset charset) {
+        this(new InputOf(text, charset), new OutputTo(path));
+    }
+
+    /**
+     * Ctor.
+     * @param text The source
+     * @param path The output path
+     * @param charset Charset
+     * @since 0.13.3
+     */
+    public TeeInput(final Text text, final Path path,
+        final CharSequence charset) {
+        this(new InputOf(text, charset), new OutputTo(path));
+    }
+
+    /**
+     * Ctor.
+     * @param text The source
+     * @param file The output file
+     * @since 0.13.3
+     */
+    public TeeInput(final Text text, final File file) {
+        this(new InputOf(text), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param text The source
+     * @param file The output file
+     * @param charset Charset
+     * @since 0.13.3
+     */
+    public TeeInput(final Text text, final File file, final Charset charset) {
+        this(new InputOf(text, charset), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param text The source
+     * @param file The output file
+     * @param charset Charset
+     * @since 0.13.3
+     */
+    public TeeInput(final Text text, final File file,
+        final CharSequence charset) {
+        this(new InputOf(text, charset), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param text The source
+     * @param output The output
+     * @since 0.13.3
+     */
+    public TeeInput(final Text text, final Output output) {
+        this(new InputOf(text), output);
+    }
+
+    /**
+     * Ctor.
+     * @param text The source
+     * @param output The output
+     * @param charset Charset
+     * @since 0.13.3
+     */
+    public TeeInput(final Text text, final Output output,
+        final Charset charset) {
+        this(new InputOf(text, charset), output);
+    }
+
+    /**
+     * Ctor.
+     * @param text The source
+     * @param output The output
+     * @param charset Charset
+     * @since 0.13.3
+     */
+    public TeeInput(final Text text, final Output output,
+        final CharSequence charset) {
+        this(new InputOf(text, charset), output);
+    }
+
+    /**
+     * Ctor.
+     * @param bytes The source
+     * @param path The output path
+     * @since 0.13.3
+     */
+    public TeeInput(final Bytes bytes, final Path path) {
+        this(new InputOf(bytes), new OutputTo(path));
+    }
+
+    /**
+     * Ctor.
+     * @param bytes The source
+     * @param file The output file
+     * @since 0.13.3
+     */
+    public TeeInput(final Bytes bytes, final File file) {
+        this(new InputOf(bytes), new OutputTo(file));
+    }
+
+    /**
+     * Ctor.
+     * @param bytes The source
+     * @param output The output
+     * @since 0.13.3
+     */
+    public TeeInput(final Bytes bytes, final Output output) {
+        this(new InputOf(bytes), output);
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param path The output path
+     * @since 0.13.3
+     */
+    public TeeInput(final Input input, final Path path) {
+        this(input, new OutputTo(path));
+    }
+
+    /**
+     * Ctor.
+     * @param input The source
+     * @param file The output file
+     * @since 0.13.3
+     */
+    public TeeInput(final Input input, final File file) {
+        this(input, new OutputTo(file));
     }
 
     /**
