271ff6bde55828ff2a9a71e75d4ed4caab4321df
Sven Diedrichsen <sdiedrichsen@yahoo.de>
2017-12-11 15:50:21 +0100

    #444 adding DateOf implementation

---
+++ src/main/java/org/cactoos/time/DateOf.java
@@ -0,0 +1,99 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cactoos.time;
+
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.Locale;
+import org.cactoos.Scalar;
+import org.cactoos.scalar.UncheckedScalar;
+
+/**
+ * Parser for date instances.
+ * @author Sven Diedrichsen (sven.diedrichsen@gmail.com)
+ * @version $Id$
+ * @since 1.0
+ */
+public class DateOf implements Scalar<TemporalAccessor> {
+
+    /**
+     * The parsed date.
+     */
+    private UncheckedScalar<TemporalAccessor> parsed;
+
+    /**
+     * Parses the date using the provided format and the zone.
+     * @param date The date to parse.
+     * @param format The format to use.
+     * @param zone The zone to use.
+     */
+    public DateOf(final String date, final String format, final ZoneId zone) {
+        this(date, DateTimeFormatter.ofPattern(format).withZone(zone));
+    }
+
+    /**
+     * Parses the provided date using the custom format and the provided locale.
+     * @param date The date to parse.
+     * @param format The custom format to use.
+     * @param locale The local to interpret the format.
+     */
+    public DateOf(final String date, final String format, final Locale locale) {
+        this(date, DateTimeFormatter.ofPattern(format).withLocale(locale));
+    }
+
+    /**
+     * Parses the date using the provided format.
+     * @param date The date to parse.
+     * @param format The format to use.
+     */
+    public DateOf(final String date, final String format) {
+        this(date, DateTimeFormatter.ofPattern(format));
+    }
+
+    /**
+     * Parses the provided date as ISO formatted.
+     * @param date The date to parse.
+     */
+    public DateOf(final String date) {
+        this(date, DateTimeFormatter.ISO_DATE_TIME);
+    }
+
+    /**
+     * Parsing the date using the provided formatter.
+     * @param date The date to parse.
+     * @param formatter The formatter to use.
+     */
+    public DateOf(final String date, final DateTimeFormatter formatter) {
+        this.parsed = new UncheckedScalar<>(
+            () -> formatter.parse(date)
+        );
+    }
+
+    @Override
+    public final TemporalAccessor value() throws Exception {
+        return this.parsed.value();
+    }
+
+}
