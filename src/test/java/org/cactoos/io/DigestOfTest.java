d52b24f51ff0e9b86b835ff2b57142e2c3329abc
Fabricio Cabral <fabriciofx@gmail.com>
2018-01-03 01:15:57 -0300

    #515 DigestOf (MD5, SHA-1 and SHA-256)

---
+++ src/test/java/org/cactoos/io/DigestOfTest.java
@@ -0,0 +1,132 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cactoos.io;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import org.cactoos.TextHasString;
+import org.cactoos.text.FormattedText;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+/**
+ * Test case for {@link DigestOf}.
+ *
+ * @author Fabricio Cabral (fabriciofx@gmail.com)
+ * @version $Id$
+ * @since 0.28
+ * @checkstyle JavadocMethodCheck (500 lines)
+ */
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+public final class DigestOfTest {
+
+    @Test
+    // @checkstyle MethodNameCheck (1 line)
+    public void md5() throws IOException {
+        MatcherAssert.assertThat(
+            "Can't calculate the MD5 checksum",
+            new FormattedText(
+                "%1$032x",
+                new BigInteger(
+                    1,
+                    new DigestOf(
+                        new InputOf("Hi Guys!"),
+                        "MD5"
+                    ).asBytes()
+                )
+            ),
+            new TextHasString(
+                "371fda3e6a3104dcbc5fabd85a2aea42"
+            )
+        );
+    }
+
+    @Test
+    // @checkstyle MethodNameCheck (1 line)
+    public void sha1() throws IOException {
+        MatcherAssert.assertThat(
+            "Can't calculate the SHA-1 checksum",
+            new FormattedText(
+                "%1$032x",
+                new BigInteger(
+                    1,
+                    new DigestOf(
+                        new InputOf("Elegant Objects!"),
+                        "SHA-1"
+                    ).asBytes()
+                )
+            ),
+            new TextHasString(
+                "17b44115042086bd5f43397552374932614e333d"
+            )
+        );
+    }
+
+    @Test
+    // @checkstyle MethodNameCheck (1 line)
+    public void sha256() throws IOException {
+        MatcherAssert.assertThat(
+            "Can't calculate the SHA-256 checksum",
+            new FormattedText(
+                "%1$032x",
+                new BigInteger(
+                    1,
+                    new DigestOf(
+                        new InputOf("Hello World!")
+                    ).asBytes()
+                )
+            ),
+            new TextHasString(
+                // @checkstyle LineLengthCheck (1 lines)
+                "7f83b1657ff1fc53b92dc18148a1d65dfc2d4b1fa3d677284addd200126d9069"
+            )
+        );
+    }
+
+    @Test
+    // @checkstyle MethodNameCheck (1 line)
+    public void sha256File() throws IOException {
+        MatcherAssert.assertThat(
+            "Can't calculate the file SHA-256 checksum",
+            new FormattedText(
+                "%1$032x",
+                new BigInteger(
+                    1,
+                    new DigestOf(
+                        new InputOf(
+                            new ResourceOf(
+                                "org/cactoos/io/DigestOf.class"
+                            ).stream()
+                        )
+                    ).asBytes()
+                )
+            ),
+            new TextHasString(
+                // @checkstyle LineLengthCheck (1 lines)
+                "d18872897e261f968124a22a7e1e36b7bb7e64ead93bbc574111cec870878077"
+            )
+        );
+    }
+
+}
