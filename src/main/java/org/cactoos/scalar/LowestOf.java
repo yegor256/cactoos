6e9358faddfdbf0ee5c3214c149d5dbca3746c41
Eduard Balovnev <bedward70@mail.ru>
2018-02-09 21:59:17 +0600

    460 fixing of findings of LowestOf class

--- src/main/java/org/cactoos/scalar/LowestOf.java
+++ src/main/java/org/cactoos/scalar/LowestOf.java
@@ -25,33 +25,63 @@ package org.cactoos.scalar;
 
 import org.cactoos.Scalar;
 import org.cactoos.iterable.IterableOf;
+import org.cactoos.iterable.Mapped;
 
 /**
- * Find the smaller among items.
+ * Find the lowest item.
  *
- * <p>There is no thread-safety guarantee.
+ * <p>Here is how you can use it to
+ * find lowest of {@link Comparable} items:</p>
+ *
+ * <pre>
+ * final String lowest = new LowestOf&lt;String&gt;(
+ *         () -&gt; "Banana", () -&gt; "Apple", () -&gt; "Orange"
+ *     ).value();
+ * // -&gt; lowest == "Apple"
+ *
+ * final Character lowestChar = new LowestOf&lt;&gt;('B', 'U', 'G').value();
+ * // -&gt; lowestChar == 'B'
+ * </pre>
  *
  * <p>This class implements {@link Scalar}, which throws a checked
  * {@link Exception}. This may not be convenient in many cases. To make
  * it more convenient and get rid of the checked exception you can
  * use {@link UncheckedScalar} or {@link IoCheckedScalar} decorators.</p>
  *
+ * <p>There is no thread-safety guarantee.
+ *
  * @author Fabricio Cabral (fabriciofx@gmail.com)
  * @author Eduard Balovnev (bedward70@mail.ru)
  * @version $Id$
  * @param <T> Scalar type
- * @since 0.9
+ * @see UncheckedScalar
+ * @see IoCheckedScalar
+ * @since 0.29
  */
 public final class LowestOf<T extends Comparable<T>> implements Scalar<T> {
 
     /**
-     * Items.
+     * Result.
      */
     private final Scalar<T> result;
 
     /**
      * Ctor.
-     * @param scalars The items
+     * @param items The comparable items
+     */
+    @SafeVarargs
+    public LowestOf(final T... items) {
+        this(
+            new Mapped<>(
+                item -> () -> item,
+                items
+            )
+        );
+    }
+
+    /**
+     * Ctor.
+     * @param scalars The scalars
      */
     @SafeVarargs
     public LowestOf(final Scalar<T>... scalars) {
