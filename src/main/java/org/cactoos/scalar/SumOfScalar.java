d237cec182fc72382018ccb7debf9d9764cc44fa
Nikita_Salomatin <Nikita_Salomatin@epam.com>
2018-02-15 15:30:04 +0400

    #619 Implemented SumOfScalar to sum up values from its scalars

---
+++ src/main/java/org/cactoos/scalar/SumOfScalar.java
@@ -0,0 +1,92 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2018 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cactoos.scalar;
+
+import java.util.stream.Collectors;
+import org.cactoos.Scalar;
+import org.cactoos.collection.CollectionOf;
+import org.cactoos.iterable.IterableOf;
+
+/**
+ * Make a scalar which is sum of scalar's values.
+ *
+ * <p>Here is how you can use it to summarize numbers:</p>
+ *
+ * <pre>
+ * int sum = new SumOfScalar(() -> 1,() -> 2, () -> 3).value(); //equal to 6
+ * </pre>
+ *
+ * <p>This class implements {@link Scalar}, which throws a checked
+ * {@link Exception}. This may not be convenient in many cases. To make
+ * it more convenient and get rid of the checked exception you can
+ * use {@link UncheckedScalar} or {@link IoCheckedScalar} decorators.</p>
+ *
+ * <p>There is no thread-safety guarantee.
+ *
+ * @author Nikita Salomatin (nsalomatin@hotmail.com)
+ * @version $Id$
+ * @since 0.9
+ */
+@SuppressWarnings(
+    {
+        "PMD.CallSuperInConstructor",
+        "PMD.OnlyOneConstructorShouldDoInitialization",
+        "PMD.ConstructorOnlyInitializesOrCallOtherConstructors"
+    }
+)
+public final class SumOfScalar implements Scalar<Integer> {
+
+    /**
+     * Serialization marker.
+     */
+    private static final long serialVersionUID = 7775359972001208404L;
+
+    /**
+     * Varargs of Scalar to sum up values from.
+     */
+    private final Scalar<Integer>[] scalars;
+
+    /**
+     * Ctor.
+     * @param src Varargs of Scalar to sum up values from
+     * @since 0.22
+     */
+    @SafeVarargs
+    public SumOfScalar(final Scalar<Integer>... src) {
+        this.scalars = src;
+    }
+
+    @Override
+    public Integer value() throws Exception {
+        return new SumOf(
+            new IterableOf<>(
+                new CollectionOf<>(this.scalars)
+                    .stream()
+                    .map(UncheckedScalar::new)
+                    .map(UncheckedScalar::value)
+                    .collect(Collectors.toList())
+            )
+        ).intValue();
+    }
+}
