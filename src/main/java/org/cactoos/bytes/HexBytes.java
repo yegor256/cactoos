4b438793fa3e9945336a650f291b59707f676e91
Alexander Menshikov <sharplermc@gmail.com>
2018-03-10 11:45:37 +0300

    fix exception message

--- src/main/java/org/cactoos/bytes/HexBytes.java
+++ src/main/java/org/cactoos/bytes/HexBytes.java
@@ -29,7 +29,7 @@ import org.cactoos.Bytes;
 import org.cactoos.Text;
 
 /**
- * Decodes origin {@link Text} using the hex encoding scheme.
+ * Decodes origin {@link Text} using the hexadecimal encoding scheme.
  *
  * @author Alexander Menshikov (sharplermc@gmail.com)
  * @version $Id$
@@ -38,14 +38,14 @@ import org.cactoos.Text;
  */
 public final class HexBytes implements Bytes {
     /**
-     * Origin hex text.
+     * Origin hexadecimal text.
      */
     private final Text origin;
 
     /**
      * Ctor.
      *
-     * @param origin Hex text.
+     * @param origin Hexadecimal text.
      */
     public HexBytes(final Text origin) {
         this.origin = origin;
@@ -54,24 +54,34 @@ public final class HexBytes implements Bytes {
     @Override public byte[] asBytes() throws IOException {
         final String hex = this.origin.asString();
         if ((hex.length() & 1) == 1) {
-            throw new IOException("Length of hex text is odd");
+            throw new IOException("Length of hexadecimal text is odd");
         }
         final byte[] result = new byte[hex.length() / 2];
         int index = 0;
         while (index < hex.length()) {
-            final int most = Character.digit(hex.charAt(index), 16);
-            final int less = Character.digit(hex.charAt(index + 1), 16);
-            if (less == -1 || most == -1) {
-                throw new IOException(
-                    new StringBuilder()
-                        .append("Unexpected character in hex string '")
-                        .append(
-                            hex.charAt(index + 1)
-                        ).append('\'').toString()
-                );
-            }
+            final int most = checkedDigit(hex.charAt(index));
+            final int less = checkedDigit(hex.charAt(index + 1));
             result[index >>> 1] = (byte) ((most << 4) + less);
             index += 2;
+        }
+        return result;
+    }
+
+    /**
+     * Wrap around {@link Character#digit(char, int)}.
+     *
+     * @param character Hexadecimal character.
+     * @return Numeric Value of the hex character.
+     * @throws IOException If fails.
+     */
+    private static int checkedDigit(final char character) throws IOException {
+        final int result = Character.digit(character, 16);
+        if (result == -1) {
+            throw new IOException(
+                new StringBuilder()
+                    .append("Unexpected character in hex string '")
+                    .append(character).append('\'').toString()
+            );
         }
         return result;
     }
