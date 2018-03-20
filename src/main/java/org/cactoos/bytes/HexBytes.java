bcbcf10a62de923fffee3834bb051c40fe9d228e
Alexander Menshikov <sharplermc@gmail.com>
2018-03-20 18:49:55 +0300

    fix codestyle

--- src/main/java/org/cactoos/bytes/HexBytes.java
+++ src/main/java/org/cactoos/bytes/HexBytes.java
@@ -27,6 +27,7 @@ package org.cactoos.bytes;
 import java.io.IOException;
 import org.cactoos.Bytes;
 import org.cactoos.Text;
+import org.cactoos.text.FormattedText;
 
 /**
  * Decodes origin {@link Text} using the hexadecimal encoding scheme.
@@ -51,7 +52,8 @@ public final class HexBytes implements Bytes {
         this.origin = origin;
     }
 
-    @Override public byte[] asBytes() throws IOException {
+    @Override
+    public byte[] asBytes() throws IOException {
         final String hex = this.origin.asString();
         if ((hex.length() & 1) == 1) {
             throw new IOException("Length of hexadecimal text is odd");
@@ -59,8 +61,8 @@ public final class HexBytes implements Bytes {
         final byte[] result = new byte[hex.length() / 2];
         int index = 0;
         while (index < hex.length()) {
-            final int most = checkedDigit(hex.charAt(index));
-            final int less = checkedDigit(hex.charAt(index + 1));
+            final int most = this.checkedDigit(hex.charAt(index));
+            final int less = this.checkedDigit(hex.charAt(index + 1));
             result[index >>> 1] = (byte) ((most << 4) + less);
             index += 2;
         }
@@ -74,13 +76,15 @@ public final class HexBytes implements Bytes {
      * @return Numeric Value of the hex character.
      * @throws IOException If fails.
      */
-    private static int checkedDigit(final char character) throws IOException {
+    private int checkedDigit(final char character) throws IOException {
         final int result = Character.digit(character, 16);
         if (result == -1) {
             throw new IOException(
-                new StringBuilder()
-                    .append("Unexpected character in hex string '")
-                    .append(character).append('\'').toString()
+                new FormattedText(
+                    "Unexpected character '%c' in hex string '%s'",
+                    character,
+                    this.origin
+                ).asString()
             );
         }
         return result;
