be7d3acbef5972a9a13aee322eb99283b79e148d
Alexander Menshikov <sharplermc@gmail.com>
2018-03-23 10:05:34 +0300

    remove CheckedDigit

--- src/main/java/org/cactoos/bytes/HexBytes.java
+++ src/main/java/org/cactoos/bytes/HexBytes.java
@@ -25,9 +25,13 @@
 package org.cactoos.bytes;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Iterator;
 import org.cactoos.Bytes;
 import org.cactoos.Text;
-import org.cactoos.iterator.CheckedDigit;
+import org.cactoos.iterator.IteratorChar;
+import org.cactoos.iterator.Mapped;
+import org.cactoos.text.FormattedText;
 
 /**
  * Decodes origin {@link Text} using the hexadecimal encoding scheme.
@@ -58,14 +62,32 @@ public final class HexBytes implements Bytes {
         if ((hex.length() & 1) == 1) {
             throw new IOException("Length of hexadecimal text is odd");
         }
-        final CheckedDigit iter = new CheckedDigit(16, hex);
+        final Iterator<Integer> iter = new Mapped<>(
+            c -> {
+                final int result = Character.digit(c, 16);
+                if (result == -1) {
+                    throw new IOException(
+                        new FormattedText(
+                            "Unexpected character '%c'",
+                            c
+                        ).asString()
+                    );
+                }
+                return result;
+            },
+            new IteratorChar(hex.toCharArray())
+        );
         final byte[] result = new byte[hex.length() / 2];
         int index = 0;
         while (index < hex.length()) {
-            final int most = iter.checkedNext();
-            final int less = iter.checkedNext();
-            result[index >>> 1] = (byte) ((most << 4) + less);
-            index += 2;
+            try {
+                final int most = iter.next();
+                final int less = iter.next();
+                result[index >>> 1] = (byte) ((most << 4) + less);
+                index += 2;
+            } catch (final UncheckedIOException ex) {
+                throw ex.getCause();
+            }
         }
         return result;
     }
