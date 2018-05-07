2df24c0e7b3389c5fe0c67a59b2f56dac9dcb63a
Paulo Lobo <pauloeduardolobo@gmail.com>
2018-05-07 10:35:04 -0300

    Using Text as Map key is impossible #788.
    Created TextEnvelope and TextEnvelopeTest for Text comparison with
    String values.

---
+++ src/main/java/org/cactoos/text/TextEnvelope.java
@@ -0,0 +1,76 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2018 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cactoos.text;
+
+import java.io.IOException;
+import org.cactoos.Text;
+
+/**
+ * Text envelope for comparisons.
+ * @author Paulo Lobo (pauloeduardolobo@gmail.com)
+ * @version $Id$
+ * @since 0.31
+ */
+public final class TextEnvelope implements Text {
+
+    /**
+     * Enveloped {@link Text}.
+     */
+    private final Text origin;
+
+    /**
+     * Ctor.
+     * @param origin Text to be envelooped.
+     */
+    public TextEnvelope(final Text origin) {
+        this.origin = origin;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 0;
+        try {
+            result = this.origin.asString().hashCode();
+        } catch (final IOException ioexc) {
+            result = -1;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        boolean result;
+        try {
+            result = this.origin.asString().equals(obj);
+        } catch (final IOException ioexc) {
+            result = false;
+        }
+        return result;
+    }
+
+    @Override
+    public String asString() throws IOException {
+        return this.origin.asString();
+    }
+}
