e4f56a7d8e865292ceeb1a47837442e97be22778
Roman Proshin <roman@proshin.org>
2018-05-07 20:04:34 +0300

    #790: Create RandomText

---
+++ src/test/java/org/cactoos/text/RandomTextTest.java
@@ -0,0 +1,84 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2018 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cactoos.text;
+
+import org.cactoos.list.ListOf;
+import org.cactoos.matchers.TextHasString;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.hamcrest.core.IsEqual;
+import org.junit.Test;
+
+/**
+ * Test for {@link RandomText}.
+ *
+ * <p>There is no thread-safety guarantee.
+ *
+ * @author Roman Proshin (roman@proshin.org)
+ * @version $Id$
+ * @since 0.32
+ * @checkstyle JavadocMethodCheck (500 lines)
+ * @checkstyle MagicNumberCheck (500 lines)
+ */
+public final class RandomTextTest {
+
+    @Test
+    public void generatesRandomTextOfRandomLength() throws Exception {
+        MatcherAssert.assertThat(
+            "Generated text is empty",
+            new RandomText().asString().length(),
+            Matchers.greaterThan(0)
+        );
+    }
+
+    @Test
+    public void generatesRandomTextOfSpecifiedLength() throws Exception {
+        MatcherAssert.assertThat(
+            "Generated text has incorrect length",
+            new RandomText(512).asString().length(),
+            new IsEqual<>(512)
+        );
+    }
+
+    @Test
+    public void generatesRandomTextOfSpecifiedChars() throws Exception {
+        MatcherAssert.assertThat(
+            "Generated text contains not allowed characters",
+            new RandomText(new ListOf<>('a'))
+                .asString()
+                .replaceAll("a", "")
+                .length(),
+            new IsEqual<>(0)
+        );
+    }
+
+    @Test
+    public void generatesRandomTextOfSpecifiedCharsAndLength() {
+        MatcherAssert.assertThat(
+            "Generated text doesn't match specification",
+            new RandomText(new ListOf<>('a'), 10),
+            new TextHasString("aaaaaaaaaa")
+        );
+    }
+}
