c9b9b580c7ef45725232362bbaa2e335eaa08b00
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2018-05-08 17:08:14 +0200

    #793 added FirstOf

---
+++ src/test/java/org/cactoos/scalar/FirstOfTest.java
@@ -0,0 +1,86 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2018 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cactoos.scalar;
+
+import org.cactoos.iterable.IterableOf;
+import org.cactoos.iterable.IterableOfInts;
+import org.cactoos.matchers.ScalarHasValue;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+/**
+ * Tests for {@link FirstOf}.
+ *
+ * <p>This class is thread-safe.
+ *
+ * @author Krzysztof Krason (Krzysztof.Krason@gmail.com)
+ * @version $Id$
+ * @since 0.31
+ * @checkstyle JavadocMethodCheck (500 lines)
+ */
+public final class FirstOfTest {
+
+    @Test
+    public void returnsMatchingValue() {
+        final int value = 1;
+        MatcherAssert.assertThat(
+            "Didn't return the only matching element",
+            new FirstOf<>(
+                i -> i >= value,
+                new IterableOfInts(0, value),
+                () -> -1
+            ),
+            new ScalarHasValue<>(value)
+        );
+    }
+
+    @Test
+    public void returnsFirstValueForMultipleMatchingOnes() {
+        final String value = "1";
+        MatcherAssert.assertThat(
+            "Didn't return first matching element",
+            new FirstOf<>(
+                i -> !i.isEmpty(),
+                new IterableOf<>("1", "2"),
+                () -> ""
+            ),
+            new ScalarHasValue<>(value)
+        );
+    }
+
+    @Test
+    public void returnsFallbackIfNothingMatches() {
+        final String value = "abc";
+        MatcherAssert.assertThat(
+            "Didn't return fallback",
+            new FirstOf<>(
+                i -> i.length() > 2,
+                new IterableOf<>("ab", "cd"),
+                () -> value
+            ),
+            new ScalarHasValue<>(value)
+        );
+    }
+
+}
