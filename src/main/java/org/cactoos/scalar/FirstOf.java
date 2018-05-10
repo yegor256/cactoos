f075b60b62baa49d9e1fdb5bada48523dbdc1601
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2018-05-10 21:36:00 +0200

    #793 update to master

--- src/main/java/org/cactoos/scalar/FirstOf.java
+++ src/main/java/org/cactoos/scalar/FirstOf.java
@@ -1,4 +1,4 @@
-/**
+/*
  * The MIT License (MIT)
  *
  * Copyright (c) 2017-2018 Yegor Bugayenko
@@ -35,8 +35,6 @@ import org.cactoos.iterable.IterableOf;
  *
  * <p>There is no thread-safety guarantee.
  *
- * @author Krzysztof Krason (Krzysztof.Krason@gmail.com)
- * @version $Id$
  * @param <T> Type of result
  * @since 0.32
  */
