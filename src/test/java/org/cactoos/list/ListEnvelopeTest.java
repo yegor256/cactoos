f833f0e3824181fe0a1b205daf45262383789f0a
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2018-05-10 20:21:02 +0200

    #814 updated master

--- src/test/java/org/cactoos/list/ListEnvelopeTest.java
+++ src/test/java/org/cactoos/list/ListEnvelopeTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * The MIT License (MIT)
  *
  * Copyright (c) 2017-2018 Yegor Bugayenko
@@ -33,8 +33,6 @@ import org.junit.Test;
 /**
  * Test case for {@link ListEnvelope}.
  *
- * @author Krzysztof Krason (Krzysztof.Krason@gmail.com)
- * @version $Id$
  * @since 0.31
  * @todo #814:30min Implement immutable ListIterator that should
  *  be returned from ListEnvelope when calling `listIterator` method. After
