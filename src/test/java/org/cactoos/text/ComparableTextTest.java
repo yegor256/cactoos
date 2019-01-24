35b98c34ee4b03a8699d78a21b36a37c11e54569
Ilya Kharlamov <502372+ilyakharlamov@users.noreply.github.com>
2019-01-24 11:16:21 -0500

    (#1030) ComparableText is not equal to the same text (formatting)

--- src/test/java/org/cactoos/text/ComparableTextTest.java
+++ src/test/java/org/cactoos/text/ComparableTextTest.java
@@ -24,25 +24,26 @@
 package org.cactoos.text;
 
 import org.cactoos.Text;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.hamcrest.core.IsEqual;
 import org.hamcrest.core.IsNot;
 import org.junit.Test;
+import org.llorllale.cactoos.matchers.Assertion;
+import org.llorllale.cactoos.matchers.TextIs;
 
 /**
  * Test case for {@link FormattedText}.
  *
  * @since 0.27
  * @checkstyle JavadocMethodCheck (100 lines)
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
  */
 public final class ComparableTextTest {
 
     @Test
-    public void comparesWithASubtext() throws Exception {
-        MatcherAssert.assertThat(
+    public void comparesWithASubtext() {
+        new Assertion<>(
             "Can't compare sub texts",
-            new ComparableText(
+            () -> new ComparableText(
                 new TextOf(
                     "here to there"
                 )
@@ -52,36 +53,40 @@ public final class ComparableTextTest {
                     new SubText("from here to there", 5)
                 )
             ),
-            Matchers.is(0)
-        );
+            new IsEqual<>(
+                0
+            )
+        ).affirm();
     }
 
     @Test
     public void comparesToUncheckedText() {
         final String txt = "foobar";
-        MatcherAssert.assertThat(
+        new Assertion<>(
             "These UncheckedText are not equal",
-            new ComparableText(
+            () -> new ComparableText(
                 new UncheckedText(
                     new TextOf(txt)
                 )
             ).compareTo(
                 new ComparableText(new TextOf(txt))
             ),
-            Matchers.equalTo(0)
-        );
+            new IsEqual<>(
+                0
+            )
+        ).affirm();
     }
 
     @Test
     public void equalsToItself() {
         final Text text = new TextOf("text");
-        MatcherAssert.assertThat(
+        new Assertion<>(
             "Does not equal to itself",
-            text,
-            new IsEqual<>(
+            () -> text,
+            new TextIs(
                 text
             )
-        );
+        ).affirm();
     }
 
     @Test
@@ -89,20 +94,20 @@ public final class ComparableTextTest {
         final Text text = new TextOf("my text");
         final Text actual = new ComparableText(text);
         final Text expected = new ComparableText(text);
-        MatcherAssert.assertThat(
+        new Assertion<>(
             "Does not equal to a comparable text made from the same Text",
-            actual,
+            () -> actual,
             new IsEqual<>(
                 expected
             )
-        );
-        MatcherAssert.assertThat(
+        ).affirm();
+        new Assertion<>(
             "Hash codes of the equal objects are not equal",
-            actual.hashCode(),
+            actual::hashCode,
             new IsEqual<>(
                 expected.hashCode()
             )
-        );
+        ).affirm();
     }
 
     @Test
@@ -110,25 +115,45 @@ public final class ComparableTextTest {
         final Text text = new ComparableText(
             new TextOf("my value")
         );
-        MatcherAssert.assertThat(
+        new Assertion<>(
             "Is equal to the completely different object",
-            text,
+            () -> text,
             new IsNot<>(
                 new IsEqual<>(
-                    new Object()
+                    "The string is ignored"
                 )
             )
-        );
-        MatcherAssert.assertThat(
+        ).affirm();
+        new Assertion<>(
+            "Is equal to the completely different text",
+            () -> text,
+            new IsNot<>(
+                new TextIs(
+                    "The text is ignored"
+                )
+            )
+        ).affirm();
+        new Assertion<>(
             "Is equal to the different ComparableText",
-            text,
+            () -> text,
             new IsNot<>(
                 new IsEqual<>(
                     new ComparableText(
-                        new TextOf("different value")
+                        new TextOf("A different text")
                     )
                 )
             )
-        );
+        ).affirm();
+        new Assertion<>(
+            "The string is equal to the different ComparableText",
+            () -> text,
+            new IsNot<>(
+                new TextIs(
+                    new ComparableText(
+                        new TextOf("A different value")
+                    )
+                )
+            )
+        ).affirm();
     }
 }
