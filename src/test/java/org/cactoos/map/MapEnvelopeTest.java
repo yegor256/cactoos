d34c835a18a7975690e6f8023041261a4295e63b
Andrey Abakumov <wilddev@ya.ru>
2019-02-05 18:30:38 +0300

    (#893) PR comment fix, refactoring
    Inner `DerivedMapEnvelope` class introduced.
    `Assertion.affirm()` call added.
    Test `emptyMapEnvelopeShouldBeEqualToEmptyDerivedMap` added

--- src/test/java/org/cactoos/map/MapEnvelopeTest.java
+++ src/test/java/org/cactoos/map/MapEnvelopeTest.java
@@ -23,6 +23,7 @@
  */
 package org.cactoos.map;
 
+import java.util.HashMap;
 import java.util.Map;
 import org.cactoos.func.FuncOf;
 import org.hamcrest.MatcherAssert;
@@ -317,14 +318,43 @@ public final class MapEnvelopeTest {
 
     @Test
     @SuppressWarnings("unchecked")
+    public void emptyMapEnvelopeShouldBeEqualToEmptyDerivedMap() {
+        final MapEnvelope<Integer, String> base = new MapOf<>();
+        final DerivedMapEnvelope<Integer, String> derived =
+            new DerivedMapEnvelope<>(new HashMap<>());
+        new Assertion<>(
+            "EmpBase and derived MapEnvelope which are empty should be equal.",
+            () -> base,
+            new IsEqual<>(derived)
+        ).affirm();
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
     public void mapEnvelopeShouldCompareDerivedClasses() {
-        final MapEntry<Integer, String> entry = new MapEntry<>(1, "one");
+        final int key = 1;
+        final String value = "one";
+        final MapEntry<Integer, String> entry = new MapEntry<>(key, value);
         final MapEnvelope<Integer, String> base = new MapOf<>(entry);
-        final Solid<Integer, String> derived = new Solid<>(entry);
+        final Map<Integer, String> hashmap = new HashMap<>();
+        hashmap.put(key, value);
+        final DerivedMapEnvelope<Integer, String> derived =
+            new DerivedMapEnvelope<>(hashmap);
         new Assertion<>(
             "Base and derived MapEnvelope of same content should be equal.",
             () -> base,
             new IsEqual<>(derived)
-        );
+        ).affirm();
+    }
+
+    /**
+     * Class derived from MapEnvelope to use in some tests.
+     * @param <K> - key type
+     * @param <V> - value type
+     */
+    private static class DerivedMapEnvelope<K, V> extends MapEnvelope<K, V> {
+        DerivedMapEnvelope(final Map<K, V> content) {
+            super(() -> content);
+        }
     }
 }
