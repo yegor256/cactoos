bda5de1cc8c42ddcfadf8b4318f6aad4c9c6e252
Victor Noël <victor.noel@crazydwarves.org>
2019-03-02 18:44:43 +0100

    (#901) Null-free ListIterator in list.NoNulls

--- src/test/java/org/cactoos/list/ListIteratorNoNullsTest.java
+++ src/test/java/org/cactoos/list/ListIteratorNoNullsTest.java
@@ -23,98 +23,27 @@
  */
 package org.cactoos.list;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ListIterator;
 import org.junit.Test;
 import org.llorllale.cactoos.matchers.Assertion;
 import org.llorllale.cactoos.matchers.Throws;
 
 /**
- * Test cases for {@link NoNulls}.
+ * Test cases for {@link ListIteratorNoNulls}.
+ *
  * @since 0.35
  * @checkstyle JavadocMethodCheck (500 lines)
  * @checkstyle MagicNumberCheck (500 lines)
  */
-public final class NoNullsTest {
+public final class ListIteratorNoNullsTest {
 
-    @Test
-    public void getThrowsErrorIfNull() {
-        new Assertion<>(
-            "must throw error if contains null",
-            () -> new NoNulls<>(
-                new ListOf<>(1, null, 3)
-            ).get(1),
-            new Throws<>(
-                "Item #1 of [1, null, 3] is NULL",
-                IllegalStateException.class
-            )
-        ).affirm();
-    }
-
-    @Test
-    public void setThrowsErrorIfArgumentNull() {
-        new Assertion<>(
-            "must throw error if set null",
-            () -> new NoNulls<>(
-                new ListOf<>(1, null, 3)
-            ).set(2, null),
-            new Throws<>(
-                "Item can't be NULL in #set(2,T)",
-                IllegalArgumentException.class
-            )
-        ).affirm();
-    }
-
-    @Test
-    public void setThrowsErrorIfPreviousValueNull() {
-        final ArrayList<Integer> list = new ArrayList<>(1);
-        list.add(null);
-        new Assertion<>(
-            "must throw error if previous value is null",
-            () -> new NoNulls<>(list).set(0, 2),
-            new Throws<>(
-                "Result of #set(0,T) is NULL",
-                IllegalStateException.class
-            )
-        ).affirm();
-    }
-
-    @Test
-    public void addThrowsErrorIfArgumentNull() {
-        new Assertion<>(
-            "must throw error if add null",
-            () -> {
-                new NoNulls<>(new ArrayList<>(1)).add(0, null);
-                return 0;
-            },
-            new Throws<>(
-                "Item can't be NULL in #add(0,T)",
-                IllegalArgumentException.class
-            )
-        ).affirm();
-    }
-
-    @Test
-    public void removeThrowsErrorIfValueNull() {
-        final ArrayList<Integer> list = new ArrayList<>(1);
-        list.add(null);
-        new Assertion<>(
-            "must throw error if removed value is null",
-            () -> new NoNulls<>(list).remove(0),
-            new Throws<>(
-                "Result of #remove(0) is NULL",
-                IllegalStateException.class
-            )
-        ).affirm();
-    }
     @Test
     public void getThrowsErrorIfListIteratorNextValueIsNullValue() {
         new Assertion<>(
-            "must throw error if removed value in iterator is null",
-            () -> new NoNulls<>(
-                new ListOf<>(null, 2, 3)
-            ).listIterator().next(),
+            "must throw error if removed value is null",
+            () -> new ListIteratorNoNulls<>(
+                new ListOf<>(null, 2, 3).listIterator()
+            ).next(),
             new Throws<>(
                 "Next item is NULL",
                 IllegalStateException.class
@@ -124,17 +53,12 @@ public final class NoNullsTest {
 
     @Test
     public void getThrowsErrorIfListIteratorPreviousValueIsNullValue() {
-        final List<Integer> list = new ArrayList<>(2);
-        list.add(1);
-        list.add(2);
-        final ListIterator<Integer> listiterator = new NoNulls<>(
-            list
-        ).listIterator();
+        final ListIterator<Integer> listiterator =
+            new ListOf<>(null, 2, 3).listIterator();
         listiterator.next();
-        list.set(0, null);
         new Assertion<>(
-            "must throw error if previous value in iterator is null",
-            () -> listiterator.previous(),
+            "must throw error if previous value is null",
+            () -> new ListIteratorNoNulls<>(listiterator).previous(),
             new Throws<>(
                 "Previous item is NULL",
                 IllegalStateException.class
@@ -147,7 +71,9 @@ public final class NoNullsTest {
         new Assertion<>(
             "must throw error if modified with add",
             () -> {
-                new NoNulls<>(new ListOf<>(1, 2, 3)).listIterator().add(4);
+                new ListIteratorNoNulls<>(
+                    new ListOf<>(1, 2, 3).listIterator()
+                ).add(4);
                 return 0;
             },
             new Throws<>(
@@ -162,7 +88,9 @@ public final class NoNullsTest {
         new Assertion<>(
             "must throw error if modified with remove",
             () -> {
-                new NoNulls<>(new ListOf<>(1, 2, 3)).listIterator().remove();
+                new ListIteratorNoNulls<>(
+                    new ListOf<>(1, 2, 3).listIterator()
+                ).remove();
                 return 0;
             },
             new Throws<>(
@@ -175,9 +103,11 @@ public final class NoNullsTest {
     @Test
     public void setThrowsErrorForImmutableListIterator() {
         new Assertion<>(
-            "must throw error if modified if set",
+            "must throw error if modified with set",
             () -> {
-                new NoNulls<>(new ListOf<>(1, 2, 3)).listIterator().set(4);
+                new ListIteratorNoNulls<>(
+                    new ListOf<>(1, 2, 3).listIterator()
+                ).set(4);
                 return 0;
             },
             new Throws<>(
