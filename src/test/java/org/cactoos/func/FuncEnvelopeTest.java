6f9f2eba26c20a0669534f81eb665f66a4683692
Victor Noël <victor.noel@crazydwarves.org>
2019-03-30 19:13:48 +0100

    (#826) Envelopes for Scalar and Func

---
+++ src/test/java/org/cactoos/func/FuncEnvelopeTest.java
@@ -0,0 +1,56 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2018 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cactoos.func;
+
+import org.cactoos.Text;
+import org.cactoos.text.FormattedText;
+import org.junit.Test;
+import org.llorllale.cactoos.matchers.Assertion;
+import org.llorllale.cactoos.matchers.FuncApplies;
+import org.llorllale.cactoos.matchers.TextIs;
+
+/**
+ * Test case for {@link FuncEnvelope}.
+ *
+ * @since 0.41
+ * @checkstyle JavadocMethodCheck (500 lines)
+ * @checkstyle JavadocTypeCheck (500 lines)
+ */
+public final class FuncEnvelopeTest {
+
+    @Test
+    public void envelopeDelegatesCalls() {
+        new Assertion<>(
+            "must delegate calls to apply",
+            () -> new Append(" stuff"),
+            new FuncApplies<>(2, new TextIs("2 stuff"))
+        ).affirm();
+    }
+
+    private static final class Append extends FuncEnvelope<Integer, Text> {
+        Append(final String suffix) {
+            super(input -> new FormattedText("%d%s", input, suffix));
+        }
+    }
+}
