69520182b3ed6c3565ead362d2741ed30afb5c09
Fevzi Anifieiev <fevzi.anifyeyev@gmail.com>
2019-09-22 22:36:16 -0400

    (#1188) Fixed @todo secription

--- src/main/java/org/cactoos/iterator/Sliced.java
+++ src/main/java/org/cactoos/iterator/Sliced.java
@@ -33,7 +33,9 @@ import java.util.Iterator;
  * <p>There is no thread-safety guarantee.</p>
  * @param <T> The type of the iterator.
  * @since 1.0.0
- * @todo #1190:30min This class to be refactored by extending IteratorEnvelope
+ * @todo #1190:30min This class to be refactored by extending IteratorEnvelope,
+ * which will provide extra functionality to subclasses and is planned to be
+ * added to Cactoos shortly.
  */
 public final class Sliced<T> implements Iterator<T> {
 
