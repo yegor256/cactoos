8215a390a93bba5415c825b33ae67ad2ef0d2962
Fevzi Anifieiev <fevzi.anifyeyev@gmail.com>
2020-03-25 12:15:03 -0400

    (#1283) Changed the matcher in unit test

--- src/test/java/org/cactoos/io/ZipTest.java
+++ src/test/java/org/cactoos/io/ZipTest.java
@@ -28,17 +28,18 @@ import java.io.File;
 import java.io.OutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import org.cactoos.Text;
 import org.cactoos.iterable.IterableOf;
+import org.cactoos.iterable.Sticky;
 import org.cactoos.iterator.Mapped;
 import org.cactoos.list.ListOf;
-import org.cactoos.scalar.Sticky;
 import org.cactoos.text.Joined;
 import org.cactoos.text.TextOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.llorllale.cactoos.matchers.Assertion;
-import org.llorllale.cactoos.matchers.ScalarHasValue;
+import org.llorllale.cactoos.matchers.HasValues;
 
 /**
  * Test case for {@link Zip}.
@@ -62,35 +63,33 @@ public final class ZipTest {
         new Assertion<>(
             "Must zip empty directory",
             new Sticky<>(
-                () -> {
-                    try (OutputStream out = new OutputStreamTo(
-                        this.temporal.newFile("empty.zip")
-                    )) {
-                        out.write(
-                            new BytesOf(
-                                new Zip(new Directory(folder))
-                            ).asBytes()
-                        );
-                    }
-                    try (ZipFile zipped = new ZipFile(
-                        new File(folder.getParentFile(), "empty.zip")
-                    )
-                    ) {
-                        return new IterableOf<>(
-                            new ListOf<>(
+                new IterableOf<>(
+                    () -> {
+                        try (OutputStream out = new OutputStreamTo(
+                            this.temporal.newFile("empty.zip")
+                        )) {
+                            out.write(
+                                new BytesOf(
+                                    new Zip(new Directory(folder))
+                                ).asBytes()
+                            );
+                        }
+                        try (ZipFile zipped = new ZipFile(
+                            new File(folder.getParentFile(), "empty.zip")
+                        )
+                        ) {
+                            return new ListOf<>(
                                 new Mapped<>(
                                     ZipEntry::getName,
                                     zipped.stream().iterator()
                                 )
-                            )
-                        );
+                            ).iterator();
+                        }
                     }
-                }
-            ),
-            new ScalarHasValue<>(
-                new IterableOf<>(
-                    "empty/"
                 )
+            ),
+            new HasValues<>(
+                "empty/"
             )
         ).affirm();
     }
@@ -110,38 +109,35 @@ public final class ZipTest {
         new Assertion<>(
             "Must zip directory with the same directory structure",
             new Sticky<>(
-                () -> {
-                    try (OutputStream out = new OutputStreamTo(
-                        this.temporal.newFile(zipname)
-                    )) {
-                        out.write(
-                            new BytesOf(
-                                new Zip(new Directory(folder))
-                            ).asBytes()
-                        );
-                    }
-                    try (ZipFile zipped = new ZipFile(
-                        new File(folder.getParentFile(), zipname)
-                    )
-                    ) {
-                        return new IterableOf<>(
-                            new ListOf<>(
+                new IterableOf<>(
+                    () -> {
+                        try (OutputStream out = new OutputStreamTo(
+                            this.temporal.newFile(zipname)
+                        )) {
+                            out.write(
+                                new BytesOf(
+                                    new Zip(new Directory(folder))
+                                ).asBytes()
+                            );
+                        }
+                        try (ZipFile zipped = new ZipFile(
+                            new File(folder.getParentFile(), zipname)
+                        )
+                        ) {
+                            return new ListOf<Text>(
                                 new Mapped<>(
                                     entry -> new TextOf(entry.getName()),
                                     zipped.stream().iterator()
                                 )
-                            )
-                        );
+                            ).iterator();
+                        }
                     }
-                }
-            ),
-            new ScalarHasValue<>(
-                new IterableOf<>(
-                    new Joined(File.separator, "abc", "A.txt"),
-                    new Joined(File.separator, "abc", "B", "B.txt"),
-                    new Joined(File.separator, "abc", "C/")
                 )
-            )
+            ),
+            new HasValues<>(
+                new Joined(File.separator, "abc", "A.txt"),
+                new Joined(File.separator, "abc", "B", "B.txt"),
+                new Joined(File.separator, "abc", "C/"))
         ).affirm();
     }
 }
