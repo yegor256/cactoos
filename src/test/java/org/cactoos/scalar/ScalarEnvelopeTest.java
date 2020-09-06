308ea2e88fcd19788afa09eb7b24318017e9de29
andreoss <andreoss@sdf.org>
2020-09-06 16:49:02 -0400

    (#1349) Extract variables

--- src/test/java/org/cactoos/scalar/ScalarEnvelopeTest.java
+++ src/test/java/org/cactoos/scalar/ScalarEnvelopeTest.java
@@ -23,6 +23,7 @@
  */
 package org.cactoos.scalar;
 
+import org.cactoos.Scalar;
 import org.hamcrest.core.IsEqual;
 import org.junit.Test;
 import org.llorllale.cactoos.matchers.Assertion;
@@ -49,15 +50,16 @@ public final class ScalarEnvelopeTest {
 
     @Test
     public void propagatesException() {
+        final String message = "ok";
+        final Scalar<Integer> scalar = () -> {
+            throw new UnsupportedOperationException(message);
+        };
         new Assertion<>(
             "must not alter the exception thrown by original Scalar",
-            new ScalarEnvelope<Integer>(
-                () -> {
-                    throw new UnsupportedOperationException("ok");
-                }) {
+            new ScalarEnvelope<Integer>(scalar) {
             },
             new Throws<>(
-                new IsEqual<>("ok"),
+                new IsEqual<>(message),
                 UnsupportedOperationException.class
             )
         ).affirm();
