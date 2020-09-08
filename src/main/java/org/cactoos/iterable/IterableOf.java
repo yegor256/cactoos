d37696119750c33cd0311f69cff9c280dbe7c304
andreoss <andreoss@sdf.org>
2020-09-08 17:11:52 -0400

    (#1183) Remove ctor & todo

--- src/main/java/org/cactoos/iterable/IterableOf.java
+++ src/main/java/org/cactoos/iterable/IterableOf.java
@@ -25,17 +25,13 @@ package org.cactoos.iterable;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
-import org.cactoos.Func;
 import org.cactoos.Scalar;
 import org.cactoos.func.FallbackFrom;
-import org.cactoos.func.UncheckedFunc;
 import org.cactoos.iterator.IteratorOf;
 import org.cactoos.scalar.And;
 import org.cactoos.scalar.Folded;
 import org.cactoos.scalar.Or;
 import org.cactoos.scalar.ScalarWithFallback;
-import org.cactoos.scalar.Sticky;
 import org.cactoos.scalar.SumOfInt;
 import org.cactoos.scalar.Unchecked;
 import org.cactoos.text.TextOf;
@@ -82,55 +78,6 @@ public final class IterableOf<X> implements Iterable<X> {
      */
     public IterableOf(final Iterator<X> list) {
         this(() -> list);
-    }
-
-    /**
-     * Paged iterable.
-     * <p>
-     * Elements will continue to be provided so long as {@code next} produces
-     * non-empty iterators.
-     * @param first First bag of elements
-     * @param next Subsequent bags of elements
-     * @param <I> Custom iterator
-     * @todo #947:30min Move this constructor in its own class with its own
-     *  tests and meaningful name (maybe Paged?). Then remove the
-     *  ClassDataAbstractionCouplingCheck suppression for IterableOf.
-     */
-    @SuppressWarnings("PMD.ConstructorOnlyInitializesOrCallOtherConstructors")
-    public <I extends Iterator<X>> IterableOf(
-        final Scalar<I> first, final Func<I, I> next
-    ) {
-        // @checkstyle AnonInnerLengthCheck (30 lines)
-        this(
-            () -> new Iterator<X>() {
-                private Unchecked<I> current = new Unchecked<>(
-                    new Sticky<>(first)
-                );
-                private final UncheckedFunc<I, I> subsequent =
-                    new UncheckedFunc<>(next);
-
-                @Override
-                public boolean hasNext() {
-                    if (!this.current.value().hasNext()) {
-                        final I next = this.subsequent.apply(
-                            this.current.value()
-                        );
-                        this.current = new Unchecked<>(
-                            new Sticky<>(() -> next)
-                        );
-                    }
-                    return this.current.value().hasNext();
-                }
-
-                @Override
-                public X next() {
-                    if (this.hasNext()) {
-                        return this.current.value().next();
-                    }
-                    throw new NoSuchElementException();
-                }
-            }
-        );
     }
 
     /**
