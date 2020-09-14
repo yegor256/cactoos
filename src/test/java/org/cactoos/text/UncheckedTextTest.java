609333a960cb13bfc8bc2ed53e1f60d414a73360
Fabrício Cabral <fabriciofx@gmail.com>
2020-09-14 03:47:51 -0300

    #1280: Add #equals() and #hashCode() to UncheckedText

--- src/test/java/org/cactoos/text/UncheckedTextTest.java
+++ src/test/java/org/cactoos/text/UncheckedTextTest.java
@@ -24,30 +24,42 @@
 package org.cactoos.text;
 
 import java.io.IOException;
+import org.cactoos.Text;
 import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNot;
+import org.hamcrest.core.StringContains;
 import org.hamcrest.object.HasToString;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.llorllale.cactoos.matchers.Assertion;
+import org.llorllale.cactoos.matchers.Throws;
 
 /**
  * Test case for {@link UncheckedText}.
  *
  * @since 0.3
  * @checkstyle JavadocMethodCheck (500 lines)
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
  */
-public final class UncheckedTextTest {
-
-    @Test(expected = RuntimeException.class)
-    public void rethrowsCheckedToUncheckedException() {
-        new UncheckedText(
-            () -> {
-                throw new IOException("intended");
-            }
-        ).asString();
+final class UncheckedTextTest {
+    @Test
+    void rethrowsCheckedToUncheckedException() {
+        final String msg = "intended";
+        new Assertion<>(
+            "Must throw an exception when something goes wrong",
+            new UncheckedText(
+                () -> {
+                    throw new IOException(msg);
+                }
+            )::asString,
+            new Throws<>(
+                new StringContains(msg),
+                RuntimeException.class
+            )
+        ).affirm();
     }
 
     @Test
-    public void toStringMustReturnSameOfAsString() {
+    void toStringMustReturnSameOfAsString() {
         final String text = "one";
         new Assertion<>(
             "Must implement #toString which returns the same of #asString",
@@ -60,4 +72,63 @@ public final class UncheckedTextTest {
         ).affirm();
     }
 
+    @Test
+    void equalsToTheSameTextObject() {
+        final Text text = new TextOf("anything");
+        new Assertion<>(
+            "Must match text representing the same value",
+            new UncheckedText(text),
+            new IsEqual<>(text)
+        ).affirm();
+    }
+
+    @Test
+    void equalsOtherTextRepresentingTheSameValue() {
+        new Assertion<>(
+            "Must match another text representing the same value",
+            new UncheckedText(
+                new TextOf("abcdefghijkl")
+            ),
+            new IsEqual<>(
+                new Joined("", "ab", "cde", "fghi", "j", "kl")
+            )
+        ).affirm();
+    }
+
+    @Test
+    void equalsNonTextObject() {
+        new Assertion<>(
+            "Must does not match another object which is not a string",
+            new UncheckedText(
+                new TextOf("is not equals to null")
+            ),
+            new IsNot<>(
+                new IsEqual<>(new Object())
+            )
+        ).affirm();
+    }
+
+    @Test
+    @SuppressWarnings("PMD.EqualsNull")
+    void notEqualsWhenAnObjectIsNull() {
+        new Assertion<>(
+            "Must match equals null",
+            new UncheckedText(
+                new TextOf("is not equals to not Text object")
+            ).equals(null),
+            new IsEqual<>(false)
+        ).affirm();
+    }
+
+    @Test
+    void matchTheSameHashCode() {
+        final String text = "hashCode";
+        new Assertion<>(
+            "Must match its represented String hashcode",
+            new UncheckedText(
+                new TextOf(text)
+            ).hashCode(),
+            new IsEqual<>(text.hashCode())
+        ).affirm();
+    }
 }
