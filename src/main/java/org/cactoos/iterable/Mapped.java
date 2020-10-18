5a15d47d7156e88587621189b65ce17e5fcd5bd7
andreoss <andreoss@sdf.org>
2020-10-19 02:07:48 +0300

    (#1169) Add a todo

--- src/main/java/org/cactoos/iterable/Mapped.java
+++ src/main/java/org/cactoos/iterable/Mapped.java
@@ -33,6 +33,10 @@ import org.cactoos.Func;
  * @param <X> Type of source item
  * @param <Y> Type of target item
  * @since 0.1
+ * @todo #1169:30m Remove type parameter X from this 
+ *   class definition, and put extra type parameter on each ctor. 
+ *   Possibly, the same change is possible in {@link iterator.Mapped},
+ *   if so, leave a todo there.
  */
 public final class Mapped<X, Y> extends IterableEnvelope<Y> {
 
