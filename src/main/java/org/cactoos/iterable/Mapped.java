6b685467845798223193b2ff1f21e5c1388f91ef
andreoss <andreoss@sdf.org>
2020-10-19 03:12:17 +0300

    (#1169) PDD compliance

--- src/main/java/org/cactoos/iterable/Mapped.java
+++ src/main/java/org/cactoos/iterable/Mapped.java
@@ -35,7 +35,7 @@ import org.cactoos.Func;
  * @since 0.1
  * @todo #1169:30m Remove type parameter X from this
  *  class definition, and put extra type parameter on each ctor.
- *  Possibly, the same change is possible in {@link iterator.Mapped},
+ *  Possibly, the same change is possible in iterator.Mapped,
  *  if so, leave a todo there after you're done with this one.
  */
 public final class Mapped<X, Y> extends IterableEnvelope<Y> {
