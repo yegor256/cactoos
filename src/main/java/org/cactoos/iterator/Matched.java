b67dd214768125f40ca86320b64f43b6c6797d12
andreoss <andreoss@sdf.org>
2020-11-01 04:57:56 +0300

    (#1481) Add license

--- src/main/java/org/cactoos/iterator/Matched.java
+++ src/main/java/org/cactoos/iterator/Matched.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2020 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.cactoos.iterator;
 
 import java.util.Iterator;
@@ -7,7 +30,7 @@ import org.cactoos.text.FormattedText;
 
 /**
  * Matched iterator.
- * 
+ *
  * Iterates over 2 iterators at the same time and verify that elements with the
  * same position has correlation by the function. The function might be equals,
  * endsWith, greaterThen, nonNull, empty, negative, positive, etc.
