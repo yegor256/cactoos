8433f068701ee7980dfab24a91b4cf5493dcb4f8
Maximillian Arruda <dearrudam@gmail.com>
2021-03-06 17:30:43 -0300

    (#1507) Fixed the annotation @since in javadoc

--- src/main/java/org/cactoos/iterable/MappedWithIndex.java
+++ src/main/java/org/cactoos/iterable/MappedWithIndex.java
@@ -31,7 +31,7 @@ import org.cactoos.BiFunc;
  * <p>
  * There is no thread-safety guarantee.
  * @param <Y> Type of target item
- * @since 0.50
+ * @since 1.0.0
  */
 public final class MappedWithIndex<Y> extends IterableEnvelope<Y> {
     /**
