37dec6fb90afabeacca90ef5f7d498ba77f1a9d7
Dmitry Barskov <dmitry.barskov64@gmail.com>
2021-05-12 12:48:55 +0300

    (#1572) Update puzzle number

--- src/main/java/org/cactoos/func/package-info.java
+++ src/main/java/org/cactoos/func/package-info.java
@@ -26,7 +26,7 @@
  * Functions.
  *
  * @since 0.1
- * @todo #1533:30min Exploit generic variance for package org.cactoos.func
+ * @todo #1572:30min Exploit generic variance for package org.cactoos.func
  *  to ensure typing works as best as possible as it is explained in
  *  #1533 issue.
  */
