3818a164b96733e2f381881a2d7fdb4d76ebbe0b
Dmitry Barskov <dmitry.barskov64@gmail.com>
2021-06-25 18:07:06 +0300

    (#1574) Generify Mapped

--- src/main/java/org/cactoos/text/Mapped.java
+++ src/main/java/org/cactoos/text/Mapped.java
@@ -25,6 +25,7 @@ package org.cactoos.text;
 
 import org.cactoos.Func;
 import org.cactoos.Text;
+import org.cactoos.func.FuncOf;
 
 /**
  * Mapped text.
@@ -39,7 +40,8 @@ public final class Mapped extends TextEnvelope {
      * @param fnc Function to apply
      * @param txt Original text
      */
-    public Mapped(final Func<String, ? extends CharSequence> fnc, final Text txt) {
+    public Mapped(final Func<? super String, ? extends CharSequence> fnc,
+        final Text txt) {
         super(new TextOf(() -> fnc.apply(txt.asString())));
     }
 
