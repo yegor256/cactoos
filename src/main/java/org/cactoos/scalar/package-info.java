6faa7717c151ea40144ded936a10161f48a7fd87
rocket <gamingdas@gmail.com>
2021-10-04 10:08:19 +0300

    (#1569) Review: Fixed the puzzle created minute rate to 30min

--- src/main/java/org/cactoos/scalar/package-info.java
+++ src/main/java/org/cactoos/scalar/package-info.java
@@ -26,7 +26,7 @@
  * Scalars.
  *
  * @since 0.12
- * @todo #1569:90min Create tests for the semantics of relaxed wildcards
+ * @todo #1569:30min Create tests for the semantics of relaxed wildcards
  *  in changed classes of {@link org.cactoos.scalar} package in #1569,
  *  which is a child of #1533.
  */
