065002897a5b100e72a136a9e7cdb5bc8831014c
h1alexbel <hialexbel@gmail.com>
2023-08-30 15:51:09 +0300

    #1175

---
+++ src/test/java/org/cactoos/scalar/ThrowsOnFalseTest.java
@@ -0,0 +1,73 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2022 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.cactoos.scalar;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test suite for {@link ThrowsOnFalse}.
+ *
+ * @since 0.56.0
+ */
+final class ThrowsOnFalseTest {
+
+    @Test
+    void throwsOnFalse() {
+        final String msg = "test message";
+        final String message =
+            Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> new ThrowsOnFalse(
+                    () -> false, msg
+                ).value()
+            ).getMessage();
+        MatcherAssert.assertThat(
+            "Exception message in right format",
+            message,
+            Matchers.equalTo(msg)
+        );
+    }
+
+    @Test
+    void returnsTrueOnTrue() throws Exception {
+        MatcherAssert.assertThat(
+            "Returns true on true statement",
+            new ThrowsOnFalse(() -> true, "test").value(),
+            Matchers.equalTo(true)
+        );
+    }
+
+    @Test
+    void doesNotThrowOnTrue() {
+        Assertions.assertDoesNotThrow(
+            () -> new ThrowsOnFalse(
+                () -> true, "msg"
+            ).value()
+        );
+    }
+}
