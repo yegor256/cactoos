cbdceddfee2852a50bba1cc90916d557ecbf684f
alimansour0002 <alimansour0002@gmail.com>
2025-03-17 21:51:52 +0300

    Implement MapDiff class for computing map differences

---
+++ src/main/java/org/cactoos/map/MapDiff.java
@@ -0,0 +1,115 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2017-2025 Yegor Bugayenko
+ * SPDX-License-Identifier: MIT
+ */
+package org.cactoos.map;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import org.cactoos.Scalar;
+import org.cactoos.iterable.IterableOf;
+import org.cactoos.scalar.Unchecked;
+
+/**
+ * Map difference.
+ *
+ * <p>There is no thread-safety guarantee.</p>
+ *
+ * @param <K> Type of key
+ * @param <V> Type of value
+ * @since 0.49
+ */
+public final class MapDiff<K, V> extends MapEnvelope<K, V> {
+
+    /**
+     * Ctor.
+     * @param first First map
+     * @param second Second map
+     * @since 0.49
+     */
+    public MapDiff(final Iterable<Map.Entry<K, V>> first,
+        final Iterable<Map.Entry<K, V>> second) {
+        super(
+            computeDiff(
+                mapFromEntries(first),
+                mapFromEntries(second)
+            )
+        );
+    }
+
+    /**
+     * Ctor.
+     * @param first First iterator
+     * @param second Second iterator
+     * @since 0.49
+     */
+    public MapDiff(final Iterator<Map.Entry<K, V>> first,
+        final Iterator<Map.Entry<K, V>> second) {
+        this(
+            new IterableOf<>(first),
+            new IterableOf<>(second)
+        );
+    }
+
+    /**
+     * Ctor.
+     * @param first First iterable supplier
+     * @param second Second iterable supplier
+     * @since 0.49
+     */
+    public MapDiff(
+        final Scalar<Iterable<Map.Entry<K, V>>> first,
+        final Scalar<Iterable<Map.Entry<K, V>>> second
+    ) {
+        this(
+            new Unchecked<>(first).value(),
+            new Unchecked<>(second).value()
+        );
+    }
+
+    /**
+     * Ctor.
+     * @param first First map
+     * @param second Second map
+     * @since 0.49
+     */
+    public MapDiff(final Map<K, V> first, final Map<K, V> second) {
+        super(computeDiff(first, second));
+    }
+
+    /**
+     * Compute the difference between two maps.
+     * @param first The first map
+     * @param second The second map
+     * @param <K> Type of keys
+     * @param <V> Type of values
+     * @return The difference map (entries in first but not in second)
+     * @since 0.49
+     */
+    private static <K, V> Map<K, V> computeDiff(
+        final Map<K, V> first,
+        final Map<K, V> second
+    ) {
+        final MapOf<K, V> result = new MapOf<>(first);
+        result.keySet().removeAll(second.keySet());
+        return result;
+    }
+
+    /**
+     * Create a map from entries.
+     * @param entries The entries
+     * @param <K> Type of keys
+     * @param <V> Type of values
+     * @return The map
+     */
+    private static <K, V> Map<K, V> mapFromEntries(
+        final Iterable<Map.Entry<K, V>> entries
+    ) {
+        final Map<K, V> map = new HashMap<>();
+        for (final Map.Entry<K, V> entry : entries) {
+            map.put(entry.getKey(), entry.getValue());
+        }
+        return map;
+    }
+}
---
+++ src/test/java/org/cactoos/map/MapDiffTest.java
@@ -0,0 +1,177 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2017-2025 Yegor Bugayenko
+ * SPDX-License-Identifier: MIT
+ */
+package org.cactoos.map;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Test;
+import org.llorllale.cactoos.matchers.Assertion;
+import org.llorllale.cactoos.matchers.HasEntry;
+
+/**
+ * Test case for {@link MapDiff}.
+ *
+ * @since 0.49
+ */
+@SuppressWarnings(
+    {
+        "PMD.AvoidDuplicateLiterals",
+        "PMD.AvoidDirectAccessToStaticFields"
+    }
+)
+final class MapDiffTest {
+
+    /**
+     * String constant for "one".
+     */
+    private static final String ONE = "one";
+
+    /**
+     * String constant for "two".
+     */
+    private static final String TWO = "two";
+
+    /**
+     * String constant for "three".
+     */
+    private static final String THREE = "three";
+
+    /**
+     * String constant for "four".
+     */
+    private static final String FOUR = "four";
+
+    /**
+     * String constant for "five".
+     */
+    private static final String FIVE = "five";
+
+    /**
+     * Tests that map difference can be computed correctly.
+     * @since 0.49
+     */
+    @Test
+    void computesMapDifference() {
+        new Assertion<>(
+            "Can't compute the difference of two maps",
+            new MapDiff<>(
+                new MapOf<>(
+                    new MapEntry<>(1, ONE),
+                    new MapEntry<>(2, TWO),
+                    new MapEntry<>(3, THREE)
+                ),
+                new MapOf<>(
+                    new MapEntry<>(2, TWO),
+                    new MapEntry<>(3, THREE),
+                    new MapEntry<>(4, FOUR)
+                )
+            ),
+            new HasEntry<>(1, ONE)
+        ).affirm();
+    }
+
+    /**
+     * Tests that map difference with empty second map returns the first map.
+     * @since 0.49
+     */
+    @Test
+    void computesMapDifferenceWithEmptySecondMap() {
+        new Assertion<>(
+            "Can't compute the difference with empty second map",
+            new MapDiff<>(
+                new MapOf<>(
+                    new MapEntry<>(1, ONE),
+                    new MapEntry<>(2, TWO),
+                    new MapEntry<>(3, THREE)
+                ),
+                new MapOf<>()
+            ),
+            new HasEntry<>(1, ONE)
+        ).affirm();
+    }
+
+    /**
+     * Tests that map difference with empty first map returns empty map.
+     * @since 0.49
+     */
+    @Test
+    void computesMapDifferenceWithEmptyFirstMap() {
+        new Assertion<>(
+            "Can't compute the difference with empty first map",
+            new MapDiff<Integer, String>(
+                new MapOf<Integer, String>(),
+                new MapOf<>(
+                    new MapEntry<>(1, ONE),
+                    new MapEntry<>(2, TWO),
+                    new MapEntry<>(3, THREE)
+                )
+            ).size(),
+            new IsEqual<>(0)
+        ).affirm();
+    }
+
+    /**
+     * Tests that map difference works with java.util.Map.
+     * @since 0.49
+     */
+    @Test
+    void computesMapDifferenceWithJavaUtilMaps() {
+        final Map<Integer, String> first = new HashMap<>();
+        first.put(1, ONE);
+        first.put(2, TWO);
+        first.put(3, THREE);
+        final Map<Integer, String> second = new HashMap<>();
+        second.put(3, THREE);
+        second.put(4, FOUR);
+        second.put(5, FIVE);
+        new Assertion<>(
+            "Can't compute the difference of two java.util.Map",
+            new MapDiff<>(first, second),
+            new HasEntry<>(1, ONE)
+        ).affirm();
+    }
+
+    /**
+     * Tests that map difference works with iterables.
+     * @since 0.49
+     */
+    @Test
+    void computesMapDifferenceWithIterables() {
+        new Assertion<>(
+            "Can't compute the difference of two iterables",
+            new MapDiff<>(
+                Collections.singletonList(new MapEntry<>(1, ONE)),
+                Collections.singletonList(new MapEntry<>(2, TWO))
+            ),
+            new HasEntry<>(1, ONE)
+        ).affirm();
+    }
+
+    /**
+     * Tests that map difference works with iterators.
+     * @since 0.49
+     */
+    @Test
+    void computesMapDifferenceWithIterators() {
+        new Assertion<>(
+            "Can't compute the difference of two iterators",
+            new MapDiff<>(
+                new MapOf<>(
+                    new MapEntry<>(1, ONE),
+                    new MapEntry<>(2, TWO),
+                    new MapEntry<>(3, THREE)
+                ).entrySet().iterator(),
+                new MapOf<>(
+                    new MapEntry<>(3, THREE),
+                    new MapEntry<>(4, FOUR),
+                    new MapEntry<>(5, FIVE)
+                ).entrySet().iterator()
+            ),
+            new HasEntry<>(1, ONE)
+        ).affirm();
+    }
+}
