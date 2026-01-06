bf29f98f1a25d8b56ddfa9d26b329bfc4ee492ce
Fabrício Barros Cabral <fabriciofx@gmail.com>
2026-01-06 11:19:05 -0300

    #1838: Update dependencies

--- src/test/java/org/cactoos/number/NumberEnvelopeTest.java
+++ src/test/java/org/cactoos/number/NumberEnvelopeTest.java
@@ -7,11 +7,10 @@ package org.cactoos.number;
 import java.util.stream.Stream;
 import org.hamcrest.core.IsEqual;
 import org.hamcrest.core.IsNot;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
-import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.llorllale.cactoos.matchers.Assertion;
 import org.llorllale.cactoos.matchers.IsTrue;
 
@@ -23,20 +22,8 @@ import org.llorllale.cactoos.matchers.IsTrue;
 @SuppressWarnings({ "serial", "PMD.TooManyMethods" })
 final class NumberEnvelopeTest implements ArgumentsProvider {
 
-    @Override
-    public Stream<Arguments> provideArguments(final ExtensionContext ctx) {
-        return Stream.of(
-            Arguments.of(4),
-            Arguments.of(8.2d),
-            Arguments.of(5.1f),
-            Arguments.of(8.2f),
-            Arguments.of((short) 2),
-            Arguments.of((byte) 9)
-        );
-    }
-
     @ParameterizedTest
-    @ArgumentsSource(NumberEnvelopeTest.class)
+    @MethodSource("arguments")
     void floatValue(final Number nbr) {
         new Assertion<>(
             "Must forward floatValue",
@@ -46,7 +33,7 @@ final class NumberEnvelopeTest implements ArgumentsProvider {
     }
 
     @ParameterizedTest
-    @ArgumentsSource(NumberEnvelopeTest.class)
+    @MethodSource("arguments")
     void intValue(final Number nbr) {
         new Assertion<>(
             "Must forward intValue",
@@ -56,7 +43,7 @@ final class NumberEnvelopeTest implements ArgumentsProvider {
     }
 
     @ParameterizedTest
-    @ArgumentsSource(NumberEnvelopeTest.class)
+    @MethodSource("arguments")
     void doubleValue(final Number nbr) {
         new Assertion<>(
             "Must forward doubleValue",
@@ -66,7 +53,7 @@ final class NumberEnvelopeTest implements ArgumentsProvider {
     }
 
     @ParameterizedTest
-    @ArgumentsSource(NumberEnvelopeTest.class)
+    @MethodSource("arguments")
     void longValue(final Number nbr) {
         new Assertion<>(
             "Must forward longValue",
@@ -76,7 +63,7 @@ final class NumberEnvelopeTest implements ArgumentsProvider {
     }
 
     @ParameterizedTest
-    @ArgumentsSource(NumberEnvelopeTest.class)
+    @MethodSource("arguments")
     void byteValue(final Number nbr) {
         new Assertion<>(
             "Must forward byteValue",
@@ -86,7 +73,7 @@ final class NumberEnvelopeTest implements ArgumentsProvider {
     }
 
     @ParameterizedTest
-    @ArgumentsSource(NumberEnvelopeTest.class)
+    @MethodSource("arguments")
     void shortValue(final Number nbr) {
         new Assertion<>(
             "Must forward shortValue",
@@ -96,7 +83,7 @@ final class NumberEnvelopeTest implements ArgumentsProvider {
     }
 
     @ParameterizedTest
-    @ArgumentsSource(NumberEnvelopeTest.class)
+    @MethodSource("arguments")
     void hashCode(final Number nbr) {
         new Assertion<>(
             "Must forward hashCode",
@@ -106,7 +93,7 @@ final class NumberEnvelopeTest implements ArgumentsProvider {
     }
 
     @ParameterizedTest
-    @ArgumentsSource(NumberEnvelopeTest.class)
+    @MethodSource("arguments")
     void equalsNumber(final Number nbr) {
         new Assertion<>(
             "Must forward equals and be true for correct Number",
@@ -116,7 +103,7 @@ final class NumberEnvelopeTest implements ArgumentsProvider {
     }
 
     @ParameterizedTest
-    @ArgumentsSource(NumberEnvelopeTest.class)
+    @MethodSource("arguments")
     void notEquals(final Number nbr) {
         new Assertion<>(
             "Must forward equals and be false for Object",
@@ -126,7 +113,7 @@ final class NumberEnvelopeTest implements ArgumentsProvider {
     }
 
     @ParameterizedTest
-    @ArgumentsSource(NumberEnvelopeTest.class)
+    @MethodSource("arguments")
     @SuppressWarnings("unlikely-arg-type")
     void notEqualsObject(final Number nbr) {
         new Assertion<>(
@@ -137,12 +124,23 @@ final class NumberEnvelopeTest implements ArgumentsProvider {
     }
 
     @ParameterizedTest
-    @ArgumentsSource(NumberEnvelopeTest.class)
+    @MethodSource("arguments")
     void toString(final Number nbr) {
         new Assertion<>(
             "Must forward toString",
             new NumberEnvelope(nbr) { }.toString(),
             new IsEqual<>(nbr.toString())
         ).affirm();
+    }
+
+    private static Stream<Arguments> arguments() {
+        return Stream.of(
+            Arguments.of(4),
+            Arguments.of(8.2d),
+            Arguments.of(5.1f),
+            Arguments.of(8.2f),
+            Arguments.of((short) 2),
+            Arguments.of((byte) 9)
+        );
     }
 }
